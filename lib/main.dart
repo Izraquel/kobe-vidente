cf59a310b1be3b1a007606ba0e0c141328d16626
Raquel Izidorio <raquelizidorio@Fabios-MacBook-Pro.local>
2023-03-07 17:12:25 -0300

    consumindo a API

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vidente_app/widgets/vidente_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() => runApp(VidenteApp());
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: Container(),
-//     );
-//   }
-// }
+Future main() async {
+  await DotEnv.load(fileName: '.env');
+  runApp(VidenteApp());
+}
